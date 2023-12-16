147c59930b7b7feac742f577b19e6f531ac48c4b
Parth Joshi <pjoshi08@github.com>
2023-12-16 00:30:25 -0500

    2d dp, greedy

---
+++ src/main/java/org/example/greedy/JumpGame.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def canJump(self, nums):
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return True if goal == 0 else False
