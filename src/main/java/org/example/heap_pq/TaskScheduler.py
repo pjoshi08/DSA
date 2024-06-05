a574a0c089ce8ba652142db42d21d733f042a865
Parth Joshi <pjoshi08@github.com>
2024-06-04 23:42:01 -0400

    minHeap

---
+++ src/main/java/org/example/heap_pq/TaskScheduler.py
@@ -0,0 +1,23 @@
+import heapq
+from collections import Counter, deque
+from typing import List
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = Counter(tasks)
+        maxHeap = [-c for c in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = deque()  # [count, availableTime]
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
+
